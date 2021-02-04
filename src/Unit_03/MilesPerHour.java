d56282b3c38341d36186016c17d8dfd6b4ec47ca
sebexp <64887829+sebexp@users.noreply.github.com>
2021-02-03 18:23:58 -0800

    Unit 03 done

---
+++ src/Unit_03/MilesPerHour.java
@@ -0,0 +1,52 @@
+package Unit_03;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes;
+	private double mph;
+
+	public MilesPerHour() {
+		setNums(0, 0, 0);
+		mph = 0.0;
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins)
+	{
+		setNums(dist, hrs, mins);
+		calcMPH();
+	}
+
+	public void setNums(int dist, int hrs, int mins)
+	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
+	}
+
+	public void calcMPH()
+	{
+		mph = distance * (minutes/60 + hours);
+	}
+
+	public void print()
+	{
+		System.out.println(toString());
+	}
+	
+	//create a print or toString or both
+	
+	public String toString()
+	{
+		return distance + " miles in " + hours + " hours and " + minutes  + " minutes = " + mph + " MPH.";
+	}
+}
